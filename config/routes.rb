489ce3c740beb8461ebd02baefa3133e28f1da07
trustpizza <maxelolsson@gmail.com>
2022-12-05 16:40:25 -0600

    Update routes to begin at events#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :events
   # Defines the root path route ("/")
   # root "articles#index"
 end

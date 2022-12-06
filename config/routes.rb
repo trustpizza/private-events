01208610c97dd348b267b4fc2eebea5b128f02ef
trustpizza <maxelolsson@gmail.com>
2022-12-05 20:14:08 -0600

    Add invitations routes to routes file;

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   
   
   devise_for :users
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events, only: [:index, :show, :create, :new]
+  resources :invitations, only: [:index, :show, :create, :new]
   # Defines the root path route ("/")
   # root "articles#index"
 end

cf7256df2d68f55c13f35681431aa86c963e9419
trustpizza <maxelolsson@gmail.com>
2022-12-05 19:36:22 -0600

    Add association between event attendees and attended-events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
+  has_many :invitations
+  has_many :attendees, through: :invitations, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
-  
+  has_many :invitations
+  has_many :attended_events, through: :invitations, source: :event
 end
